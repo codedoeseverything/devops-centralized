a7df324c378faa4af3d73f016049eefaf3cca4e3
codedoeseverything <codedoeseverything@gmail.com>
2021-03-18 23:18:34 +1100

    fix-105

--- scripts/cfn-secret-retrieve.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cat >>config/cfn.params <<EOF 
-
-$CFN_PARAM1=$(aws secretsmanager get-secret-value --secret-id $SECRET_VAR| jq --raw-output '.SecretString' | jq -r .$CFN_PARAM1)
-
-EOF
